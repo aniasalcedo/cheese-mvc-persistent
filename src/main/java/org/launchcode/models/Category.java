7c40902018886198a2f04984deb740e852396b5e
Anna Salcedo <salcedoanna8@gmail.com>
2019-11-15 15:44:02 -0500

    In the process of fixing the project

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,10 +1,10 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Category {
@@ -12,15 +12,27 @@ public class Category {
     @GeneratedValue
     private int id;
 
+    @NotNull
+    @Size(min=3, max=15)
+    private String name;
+
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
+
+    public Category() { }
+
+
+    public Category(String name) {
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
 
-    @NotNull
-    @Size (min=3,max=15)
-    private String name;
-
     public String getName() {
+
         return name;
     }
 
@@ -28,10 +40,7 @@ public class Category {
         this.name = name;
     }
 
-    public Category() { }
-
-
-    public Category(String name) {
-        this.name = name;
+    public List<Cheese> getCheeses() {
+        return cheeses;
     }
 }
